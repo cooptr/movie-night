209c773ec737ce724651399beee77c20958959c8
Cooper <cooperlewschool@gmail.com>
2026-03-11 01:00:52 -0700

    async changes

--- scrape.py
+++ scrape.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import asyncio
+import aiohttp
+import random
+
 
 #url = "https://letterboxd.com/lucas_sequeira/watchlist/page/1/"
 
@@ -22,7 +26,31 @@ def soupify(url):
 
         else:
             print(f"Failed to retrieve the page. Status code: {response.status_code}")
+            return None
 
     except requests.exceptions.RequestException as e:
         print(f"An error occured: {e}") 
-   
+        return None
+
+async def grab(url, sem):
+
+    random_time = random.uniform(1.0, 3.0)
+    await asyncio.sleep(random_time)
+
+    async with sem:
+
+        async with aiohttp.ClientSession() as session:
+
+            try:
+                async with session.get(url, headers=headers) as response:
+                    if response.status == 200:
+                        html = await response.text()
+                        return html
+                    else:
+                        print(f"failed to fetch {url}, status: {response.status}")
+
+            except aiohttp.ClientError as e:
+                print(f"network error: {e}")
+
+
+    
--- scrape_logic.py
+++ scrape_logic.py
@@ -1,18 +1,49 @@
 from bs4 import BeautifulSoup
 from scrape import soupify
+import math
+import asyncio
+from scrape import grab
+url = "https://letterboxd.com/bwbrewster/watchlist/page/"
 
-url = "https://letterboxd.com/lucas_sequeira/watchlist/page/2/"
+def list_page_movies(html):
 
-def list_movies(url):
+    soup = BeautifulSoup(html, 'html.parser')
     
-    soup = soupify(url)
     parent_list = soup.find(class_="grid -p125 -scaled128")
-
     movies = parent_list.find_all("div", class_="react-component")
-
     for movie in movies:
         print(movie.get('data-item-full-display-name'))
-    #print(type(parent_list.find_all("div")))
+
+async def generate_watchlist_links(url):
+
+    sem = asyncio.Semaphore(10)
+
+    first_url = url + '1/'
+    html = await grab(first_url, sem)
+    soup = BeautifulSoup(html, "html.parser")
+
+    page_count_string = soup.find(class_="js-watchlist-count").text
+    page_count = math.ceil(int(page_count_string.split('f')[0][:-1].replace(",",""))/28)
+    links = []
+    i = 0
+    while i < page_count:
+        i += 1
+        current_page_number = str(i)
+        current_page = url + current_page_number + '/'
+        links.append(current_page)
+    return(links)
+
+async def main(url):
+
+    sem = asyncio.Semaphore(10)
+
+    links = await generate_watchlist_links(url)
+    coros = [grab(url, sem) for url in links]
+    results = await asyncio.gather(*coros)
+    for result in results:
+        list_page_movies(result)
+
+asyncio.run(main(url))
 
 
-list_movies(url)
+
